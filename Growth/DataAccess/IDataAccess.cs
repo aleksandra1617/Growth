54be8de27c42acc28a24e7c783f17f1010bd8491
AleksandraPetkova <a.petkova1617@gmail.com>
2022-08-23 14:37:06 +0100

    Add the SQLite package and connect to a DB file.

--- Growth/DataAccess/IDataAccess.cs
+++ Growth/DataAccess/IDataAccess.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace DataAccess
 {
     public interface IDataAccess
     {
+        public bool Connect();
+
         public string GetData();
     }
 }
--- Growth/DataAccess/SQLiteDataAccess.cs
+++ Growth/DataAccess/SQLiteDataAccess.cs
@@ -1,13 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Extensions.Logging;
+using System.Data.SQLite;
+
 
 namespace DataAccess
 {
     public class SQLiteDataAccess : IDataAccess
     {
+        private readonly ILogger<SQLiteDataAccess> _logger;
+        private static SQLiteConnection? _connection;
+
+        public const string _databaseName = "database.sqlite3";
+        public const string _connectionString = "Data Source=" + _databaseName;
+
+        public SQLiteDataAccess(ILogger<SQLiteDataAccess> logger)
+        {
+            _logger = logger;
+
+            if (!File.Exists("./" + _databaseName))
+            {
+                _logger.LogWarning("DB file not found! Creating DB File..");
+                SQLiteConnection.CreateFile(_databaseName);
+                _logger.LogInformation("DB file created!");
+            }
+
+            _logger.LogDebug("<<SQLiteDataAccess>> Constructed Successfully.");
+        }
+
+        public bool Connect()
+        {
+            _connection = new SQLiteConnection(_connectionString);
+            _connection.Open();
+
+            if (_connection.State == System.Data.ConnectionState.Open) 
+                return true;
+
+            return false;
+        }
+
         public string GetData(){ return "Test Data"; }
     }
 }
